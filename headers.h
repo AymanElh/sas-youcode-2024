2f917dabea992711e774fa28fa460f5dca0a8e7c
ayman <aymanelh001@gmail.com>
2024-09-11 14:12:13 +0200

    Project Created

---
+++ headers.h
@@ -0,0 +1,31 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STUDENTS 1000
+#define STRING_SIZE 20
+
+
+
+typedef struct 
+{
+    int jour;
+    int mois;
+    int annee;
+}Date;
+
+typedef struct 
+{
+    int id;
+    char nom[STRING_SIZE];
+    char prenom[STRING_SIZE];
+    Date dateNaissance;
+    char departement[STRING_SIZE];
+    float note_generale;
+}Etudiant;
+
+
+
+#endif
